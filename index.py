8d6d7fe2f91541913e290bbf27c44e57317f37f6
montazeri_a <montazeri_a@office.zharfpouyan.net>
2022-03-29 17:16:18 +0330

    =new

--- index.py
+++ index.py
@@ -1,5 +1,5 @@
-from telegram.ext import Updater, CallbackContext, CommandHandler, MessageHandler, Filters
-from telegram import Update, InlineKeyboardButton
+from telegram.ext import Updater, CallbackContext, MessageHandler, Filters, CommandHandler
+from telegram import Update
 import logging
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -9,13 +9,22 @@ logger = logging.getLogger(__name__)
 token = "2016260844:AAGwWwI6ZLA7cLUNNcAbbFz2W84wkJebZyo"
 
 def start(update: Update, context: CallbackContext):
-    update.message.reply_text('h')
+    chat_id = update.message.chat_id
+    message_id = update.message.message_id
+    context.bot.send_message(chat_id=chat_id, text="hello world")
+
+def echo(update: Update, context: CallbackContext):
+    chat_id = update.message.chat_id
+    message_id = update.message_id
+    text = update.message.text
+    context.bot.send_message(chat_id, text=text)
 
 def main():
     updater = Updater(token, use_context=True)
     dispatcher = updater.dispatcher
 
-    dispatcher.add_handler(MessageHandler(Filters.text, start))
+    dispatcher.add_handler(CommandHandler(['START', 'start'], start))
+    dispatcher.add_handler(MessageHandler(Filters.text, echo))
     updater.start_polling()
     updater.idle()
 
